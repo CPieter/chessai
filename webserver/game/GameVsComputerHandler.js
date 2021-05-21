d4912c756e976c8c9bea97c7728a52a71e1ad872
KYPremco <KYPremco@gmail.com>
2021-05-21 12:04:02 +0200

    Changed fixed events to event registers

---
+++ webserver/game/GameVsComputerHandler.js
@@ -0,0 +1,82 @@
+const { Chess } = require('chess.js')
+
+module.exports = (io, socket) => {
+    const game = new Chess();
+
+    const move = (payload) => {
+        game.move(payload);
+        updateStatus(game);
+
+
+        const moves = game.moves()
+        const response = moves[Math.floor(Math.random() * moves.length)]
+
+        socket.emit("move", response);
+        game.move(response);
+        updateStatus(game);
+    }
+
+    const reset = (payload) => {
+        game.reset();
+        updateStatus(game);
+        if (payload == "black") {
+            const moves = game.moves()
+            const response = moves[Math.floor(Math.random() * moves.length)]
+            setTimeout(() => {
+                socket.emit("move", response);
+                game.move(response);
+                updateStatus(game);
+            }, 1000)
+        }
+    }
+
+    const changeSide = (payload) => {
+        game.reset();
+        updateStatus(game);
+        if (payload == "black") {
+            const moves = game.moves()
+            const response = moves[Math.floor(Math.random() * moves.length)]
+            setTimeout(() => {
+                socket.emit("move", response);
+                game.move(response);
+                updateStatus(game);
+            }, 1000)
+        }
+    }
+
+    socket.on("move", move);
+    socket.on("reset", reset);
+    socket.on("changeSide", changeSide);
+}
+
+function updateStatus(game) {
+    var status = ''
+
+    var moveColor = 'White'
+    if (game.turn() === 'b') {
+        moveColor = 'Black'
+    }
+
+    // checkmate?
+    if (game.in_checkmate()) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.'
+    }
+
+    // draw?
+    else if (game.in_draw()) {
+        status = 'Game over, drawn position'
+    }
+
+    // game still on
+    else {
+        status = moveColor + ' to move'
+
+        // check?
+        if (game.in_check()) {
+            status += ', ' + moveColor + ' is in check'
+        }
+    }
+    console.log(game.pgn());
+    console.log(status);
+
+}
--- webserver/server.js
+++ webserver/server.js
@@ -1,92 +1,9 @@
-
 const { io } = require('./server_setup');
-const { Chess } = require('chess.js')
+const registerGameVsComputerHandler = require("./game/GameVsComputerHandler.js");
 
-
-
-io.sockets.on('connection', (socket) => {
-    console.log('We have a new client: ' + socket.id);
-
-    const game = new Chess();
-    socket.on("move", (move) => {
-
-        game.move(move);
-        updateStatus(game);
-
-
-        const moves = game.moves()
-        const response = moves[Math.floor(Math.random() * moves.length)]
-
-        socket.emit("move", response);
-        game.move(response);
-        updateStatus(game);
-
-    });
-
-    socket.on("reset", (side) => {
-        game.reset();
-        updateStatus(game);
-        if (side == "black") {
-            const moves = game.moves()
-            const response = moves[Math.floor(Math.random() * moves.length)]
-            setTimeout(() => {
-                socket.emit("move", response);
-                game.move(response);
-                updateStatus(game);
-            }, 1000)
-        }
-
-
-    });
-
-    socket.on("changeSide", (side) => {
-        game.reset();
-        updateStatus(game);
-        if (side == "black") {
-            const moves = game.moves()
-            const response = moves[Math.floor(Math.random() * moves.length)]
-            setTimeout(() => {
-                socket.emit("move", response);
-                game.move(response);
-                updateStatus(game);
-            }, 1000)
-        }
-
-
-    });
-
-});
-
-function updateStatus(game) {
-    var status = ''
-
-    var moveColor = 'White'
-    if (game.turn() === 'b') {
-        moveColor = 'Black'
-    }
-
-    // checkmate?
-    if (game.in_checkmate()) {
-        status = 'Game over, ' + moveColor + ' is in checkmate.'
-    }
-
-    // draw?
-    else if (game.in_draw()) {
-        status = 'Game over, drawn position'
-    }
-
-    // game still on
-    else {
-        status = moveColor + ' to move'
-
-        // check?
-        if (game.in_check()) {
-            status += ', ' + moveColor + ' is in check'
-        }
-    }
-    console.log(game.pgn());
-    console.log(status);
-
+const onConnection = (socket) => {
+    // registreer alle handlers bij connectie
+    registerGameVsComputerHandler(io, socket);
 }
 
-
+io.sockets.on("connection", onConnection);
