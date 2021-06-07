db19a4ecc76185e67e481e692665cc661d12a590
KYPremco <KYPremco@gmail.com>
2021-06-07 18:57:25 +0200

    Create quickmatch game, implemented game rooms

--- webserver/matchmaking/QueueHandler.js
+++ webserver/matchmaking/QueueHandler.js
@@ -1,17 +1,26 @@
 const RoomService = require("../room/RoomService.js");
 const MatchmakingService = require("./MatchmakingService.js");
+const GameRoom = require("../game/GameRooms");
+const {Chess} = require('chess.js');
+const PlayerVsPlayerHandler = require("../game/PlayerVsPlayerHandler");
+const GameSocketEvents = require("../game/GameSocketEvents");
 
 const CreateQuickMatch = (io, sockets) => {
     const roomId = RoomService.GenerateUniqueRoomString();
-    const player1 = io.sockets.sockets.get(sockets[0]);
-    const player2 = io.sockets.sockets.get(sockets[1]);
-    MatchmakingService.LeaveQueue(io, player1, "");
-    MatchmakingService.LeaveQueue(io, player2, "");
-    player1.join(roomId);
-    player2.join(roomId);
+    GameRoom.set(roomId, new Chess);
+    StartQuickMatch(io, io.sockets.sockets.get(sockets[0]), roomId, 'white');
+    StartQuickMatch(io, io.sockets.sockets.get(sockets[1]), roomId, 'black');
 
-    io.of("/").to(roomId).emit("custom", [...io.sockets.adapter.rooms.get(roomId)]);
+    //TODO: Send player information to each other
+    // io.of("/").to(roomId).emit("custom", [...io.sockets.adapter.rooms.get(roomId)]);
 };
+
+const StartQuickMatch = (io, socket, roomId, color) => {
+    MatchmakingService.LeaveQueue(io, socket, "");
+    socket.join(roomId);
+    PlayerVsPlayerHandler(io, socket, roomId);
+    socket.emit(GameSocketEvents.Start, color)
+}
 
 module.exports.init = (io) => {
     io.of("/").adapter.on("join-room", (room, socket) => {
